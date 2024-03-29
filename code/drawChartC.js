c3fc623e3bda8e5cf1d3859b7bf8b22d366c6f7c
narahari-25 <127509509+narahari-25@users.noreply.github.com>
2024-03-29 22:07:06 +0530

    Update drawChartC.js

--- code/drawChartC.js
+++ code/drawChartC.js
@@ -1,3 +1,4 @@
+/*This function draws the grouped bar chart of Food and beverages category*/
 function drawChartC(file1Data, file2Data, file3Data, file4Data) {
     d3.select("#chart").selectAll("svg").remove();
     d3.selectAll(".tooltip").remove();
@@ -100,7 +101,7 @@ function drawChartC(file1Data, file2Data, file3Data, file4Data) {
 
     // Create and append the bars for expenditure
     function barClickHandler(d) {
-
+/*When a bar clicked through this function a new stacked bar chart is shown for a particular company for all the years*/
         loadData().then(([mnm1Data, mnm2Data, mnm3Data, mnm4Data]) => {
             if (d.letter == 'GodawariPower') {
                 drawChartA2(mnm1Data);
