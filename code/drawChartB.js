1a12ae4a63ec6e584502cba535f5541dff371def
narahari-25 <127509509+narahari-25@users.noreply.github.com>
2024-03-29 22:06:02 +0530

    Update drawChartB.js

--- code/drawChartB.js
+++ code/drawChartB.js
@@ -1,3 +1,4 @@
+/*This function draws the grouped bar chart of Food and beverages category*/
 function drawChartB(file1Data, file2Data, file3Data, file4Data) {
     d3.select("#chart").selectAll("svg").remove();
     d3.selectAll(".tooltip").remove();
@@ -100,7 +101,7 @@ function drawChartB(file1Data, file2Data, file3Data, file4Data) {
 
     // Create and append the bars for expenditure
     function barClickHandler(d) {
-
+/*When a bar clicked through this function a new stacked bar chart is shown for a particular company for all the years*/
         loadData().then(([file1Data, file2Data, file3Data, file4Data, ph1Data, ph2Data, ph3Data, ph4Data]) => {
             if (d.letter == 'Hester-Bio') {
                 drawChartA2(ph1Data);
